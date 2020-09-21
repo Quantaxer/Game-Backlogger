edbd04a968141dcbd737df180410edc15f8f81c5
Quantaxer <phudel@uoguelph.ca>
2020-09-21 11:00:57 -0400

    Remove clippy

--- static/js/init.js
+++ static/js/init.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
-
-  // Load Links
-  clippy.load('Links', function(agent) {
-    // Do anything with the loaded agent
-    agent.show();
-
-    $('#artsy-button').click(function () {
-      agent.play('GetArtsy');
-    });
-  });
 	
 	//Replace with a Links animation
     $('#alert-button').click(function () {
