19286eaa0453e5be27600b8db0ea50cbc4f6e683
Quantaxer <phudel@uoguelph.ca>
2020-09-28 19:38:04 -0400

    f

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from flask import Flask, render_template, jsonify
 
 import MySQLdb
 
-db = MySQLdb.connect(host="dursley.socs.uoguelph.ca",
-    user="phudel",
-    passwd="1012673",
-    db="phudel"
-)
+db = None
+cur = None
+
 
 app = Flask(__name__, static_url_path='')
 #app.debug = True
@@ -17,6 +15,20 @@ serverObject = {"name": "serverObject"}
 
 @app.route('/')
 def index(name=None):
+
+    try:
+        db = MySQLdb.connect(host="dursley.socs.uoguelph.ca",
+            user="phudel",
+            passwd="1012673",
+            db="phudel"
+        )
+
+        cur = MySQLdb.cursor()
+        cur.execute("CREATE TABLE `users` (`key` INT NOT NULL AUTO_INCREMENT, `username` VARCHAR(255) NOT NULL, `password` VARCHAR(255) NOT NULL;")
+    except:
+        print("Error conencting and initializing the db")
+
+
     return render_template('index.html', name=name)
 
 @app.route("/showObject", methods = ["GET"])
