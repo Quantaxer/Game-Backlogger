c6796347ec583912cd7e6e99093218947cc0219e
Quantaxer <phudel@uoguelph.ca>
2020-11-09 10:23:59 -0500

    Kill all useless lab code

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Lab 8
+# Lab 89
 
-This is the Eighth lab for CIS*3210, created by Peter Hudel, SID 1012673.
+This is the Nint lab for CIS*3210, created by Peter Hudel, SID 1012673.
 
 ## What this application does
 
-This application allows a user to search for the title of a video game on Wikipedia, and the website will return a quick summary of the video game, as well as the main picture associated with it. The user can also choose to use some sample input, and they can view all the categories associated with the wikipedia page.
+This application allows a user to search for the title of a video game on Wikipedia, and the website will return a quick summary of the video game, the categories of the Wiki article, and a link to the Wiki page. It will store the result into a table. A user can click on a row in the table to select the row, dosplaying the image of the game. They can then drag the row to reorder the table, or they can press a delete button to delete the row from the table. The user can also choose to use some sample input, and they can view all the categories associated with the wikipedia page.
 
 ## How to run this application
 
-To run this application, open up a terminal in the root directory of this project. Once the terminal is open, type the command `flask run --host=0.0.0.0 --port=12673`. this is assuming that Flask is installed on your machine. If it isn't, run `pip install flask`. It also requires MYSQLdb for python, so if that also isn't installed please install it.
+To run this application, open up a terminal in the root directory of this project. Once the terminal is open, type the command `flask run --host=0.0.0.0 --port=12673`. this is assuming that Flask is installed on your machine. If it isn't, run `pip install flask`.
 
 To run it in development mode, use  `FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0 --port=12673`
 
--- app.py
+++ app.py
@@ -8,29 +8,6 @@ import MySQLdb
 # My API functions
 from api import *
 
-# CHANGE THIS EVENTUALLY ONCE NEED TO LOG IN
-alwaysLoggedIn = True
-
-"""try:
-    db = MySQLdb.connect(host="dursley.socs.uoguelph.ca",
-        user="phudel",
-        passwd="1012673",
-        db="phudel"
-    )
-
-    cur = db.cursor()
-    cur.execute("create table if not exists users(username varchar(255) not null, password varchar(255) not null, constraint unique_name unique(username), primary key(username));")
-    cur.execute("create table if not exists videogames(id int auto_increment primary key, name varchar(255) not null, summary varchar(1024) not null, ranking int not null, user varchar(255) not null, constraint unique_name unique(name), FOREIGN KEY(user) REFERENCES users(username) ON DELETE CASCADE);")
-    print("Successfully created tables")
-except:
-    print("Error connecting and initializing the db")
-
-try:
-    cur.execute("insert into users (username, password) values ('bob', '123');")
-except:
-    print("bob already exists")"""
-
-
 app = Flask(__name__, static_url_path='')
 
 app.secret_key = secrets.token_urlsafe(16)
@@ -39,127 +16,8 @@ app.secret_key = secrets.token_urlsafe(16)
 
 @app.route('/')
 def index():
-    if 'isCreatingUser' not in session:
-        session['isCreatingUser'] = False
+    return render_template('index.html')
     
-    if alwaysLoggedIn:
-        return render_template('index.html', logged_in=alwaysLoggedIn, username="bobbyboi", create_user=session['isCreatingUser'])
-    else:
-        if 'logged_in' in session:
-            return render_template('index.html', logged_in=session['logged_in'], username=session['username'], create_user=session['isCreatingUser'])
-        else:
-            return render_template('index.html', logged_in=False, username='', create_user=session['isCreatingUser'])
-
-@app.route("/login", methods = ["POST"])
-def get_db_credentials(name=None):
-    status = ""
-    username = request.form['username']
-    password = request.form['password']
-
-    try:
-        cur.execute("select * from users where username = %s and password = %s;", (username,password,))
-        result = cur.fetchall()
-        if (cur.rowcount == 1):
-
-            status = "Successfully logged in"
-            session['logged_in'] = True
-            session['username'] = result[0][0]
-        else:
-            status = "Error logging in: Please check username and password"
-    except Exception as e:
-        print(e)
-        status = "Some unknown error occurred"
-
-    return jsonify(status=status)
-
-@app.route("/switchCreateUserState", methods = ["GET"])
-def switch_create_state():
-    session['isCreatingUser'] = not session['isCreatingUser']
-    return "True"
-
-@app.route("/logout", methods = ["GET"])
-def logout():
-    session.pop('username', None)
-    session.pop('logged_in', None)
-    return "True"
-
-@app.route("/register", methods = ["POST"])
-def register_user(name=None):
-    status = ""
-    username = request.form['username']
-    password = request.form['password']
-    confirmation = request.form['confirm']
-
-    if (not password == confirmation):
-        return jsonify(status="Passwords do not match")
-
-    try:
-        cur.execute("insert into users (username, password) values (%s, %s);", (username, password,))
-        db.commit()
-        status = "Created new user"
-    except Exception as e:
-        print(e)
-        status = "User already exists"
-
-    return jsonify(status=status)
-
-
-@app.route("/query", methods = ["GET"])
-def queryObject():
-    status = ""
-    try:
-        cur.execute("select * from videogames where name='Witcher 3' and user='bob';")
-        status = "Selected " + str(cur.rowcount) + " row(s) from the database"
-        print(status)
-    except Exception as e:
-        print(e)
-        status = "Failed querying: check server code"
-    if (cur.rowcount > 0):
-        return jsonify(res=cur.fetchall(), status=status, numrows=cur.rowcount)
-    else:
-        return jsonify(res={}, status=status, numrows=cur.rowcount)
-
-
-@app.route("/add", methods = ["POST"])
-def addToObject():
-    status = ""
-    try:
-        cur.execute("insert into videogames (name, summary, ranking, user) values ('Witcher 3', 'A really good game', 1, 'bob');")
-        db.commit()
-        status = "inserted " + str(cur.rowcount) + " row(s) into the database"
-    except Exception as e:
-        print(e)
-        status = "Error adding row in database: already exists"
-        db.rollback()
-
-    return jsonify(res={}, status=status)
-
-@app.route("/modify", methods = ["PUT"])
-def modifyObject():
-    status = ""
-    try:
-        cur.execute("update videogames set ranking='2' where name='Witcher 3' and user='bob';")
-        db.commit()
-        status = "modified " + str(cur.rowcount) + " row(s) in the database"
-    except Exception as e:
-        print(e)
-        status = "Error modifying row in database"
-        db.rollback()
-    return jsonify(res={}, status=status)
-
-@app.route("/delete", methods = ["DELETE"])
-def deleteObject():
-    status = ""
-    try:
-        cur.execute("delete from videogames where user='bob' and name='Witcher 3';")
-        db.commit()
-        status = "Deleted " + str(cur.rowcount) + " row(s) from the database"
-    except Exception as e:
-        print(e)
-        status = "Failed deleting from database"
-        db.rollback()
-    return jsonify(res={}, status=status)
-
 @app.route("/searchTheWiki", methods = ["POST"])
 def searchTheWiki():
     queryTitle = request.form['title']
